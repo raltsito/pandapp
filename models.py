7e316452e9af0f8e486de080c879612b19e64816
Carlos Alberto Gonzalez Becerra <carlosbecerra@uadec.edu.mx>
2026-05-23 10:54:25 -0600

    MMMM

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 from django.utils import timezone
-
+from django.utils import timezone
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
